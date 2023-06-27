53cb8458d6b6f50bc56958798c14ee8329c9c628
pandeyrohitmanoj <41313712+pandeyrohitmanoj@users.noreply.github.com>
2023-06-27 17:38:28 +0530

    update frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
 import { BrowserRouter } from 'react-router-dom'
 
 import Routes from './Components/Routes'
 
 import './Styles/index.css'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCprWQ1vVYi97R_RZ4TZnoBdtWq92BbIkQ",
+  authDomain: "car-rental-mern.firebaseapp.com",
+  projectId: "car-rental-mern",
+  storageBucket: "car-rental-mern.appspot.com",
+  messagingSenderId: "314781834838",
+  appId: "1:314781834838:web:1f1c9ea7018d1d2c3d25f7",
+  measurementId: "G-HB7Z1BVTE1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
